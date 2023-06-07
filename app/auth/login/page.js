cd08ae5353c331cf1beba3f24ae9756503002496
Rspillere <rspillere@gmail.com>
2023-06-07 14:39:14 -0300

    login page styled and recover pass page added

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,18 +1,75 @@
 
-export default function Page() {
-    return (
-    <div className="flex-col">
-      <div className='bg-indigo-500 w-screen'>
-        <h1>Login</h1>
-      </div>
-      <div>
-        <form className="flex-col">
-          <input type="text" placeholder="user"></input>
-          <input type="password" placeholder="user"></input>
-          <button className='bg-blue-400'>Login</button>
-        </form>
+export default function Login() {
+  return (
+    <>
+      <div className="flex min-h-full flex-col justify-center">
+        <div className=" bg-sky-600 w-screen">
+          <h2 className="my-28 text-center  text-2xl font-bold leading-9 tracking-tight text-white">
+            Loginbienchingon
+          </h2>
+        </div>
 
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  placeholder="user"
+                  className="block w-full rounded-md border-0 py-1.5 px-3  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-sky-600 hover:text-sky-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  placeholder="password"
+                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="#" className="font-semibold leading-6 text-sky-600 hover:text-sky-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
-    )
-  }
+    </>
+  )
+}
---
+++ app/auth/recover/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+        <h1>Soy el recover</h1>
+        </div>
+    )
+}
